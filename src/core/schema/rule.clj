417191107fcfeaeb858e6fa79206bf450c1d9c6d
Pablo Costanzo <costanzopa@gmail.com>
2017-09-08 01:06:28 -0300

    Refactorizando Database y Rule.

--- src/core/schema/rule.clj
+++ src/core/schema/rule.clj
@@ -1,1 +1,13 @@
-(ns rule)
+(ns
+  ^{:doc "Defines rule functions."
+    :author "Pablo Costanzo"}
+  core.schema.rule
+  (:require [clojure.string :as str]))
+
+
+(def rule-assign ":-")
+
+(defn is-rule?
+  "Determines if it's a rule."
+  [line]
+  (.contains line rule-assign))
--- src/core/storage/database.clj
+++ src/core/storage/database.clj
@@ -2,4 +2,17 @@
   ^{:doc "Defines database functions."
     :author "Pablo Costanzo"}
   core.storage.database
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+   :require [core.schema.fact :refer :all]
+   :require [core.schema.rule :refer :all]))
+
+(defn convert-database-to-lines
+  "Convert a string database to a string vector."
+  [database]
+  (into [] (filter not-empty (map str/trim(str/split-lines database))))
+  )
+
+(defn validate-database [database-line]
+  "Validates if the line passed by parameter is a rule or a fact."
+  (if (is-fact? database-line) true
+      (if (is-rule? database-line) true false)))
